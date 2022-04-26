dd4a50979cc991cd606d7ffe387b4fb424f5e5ff
d <g@bk.ru>
2022-04-26 06:56:01 +0300

    I maked full geo_inp

--- Flask-site/static/js/map.js
+++ Flask-site/static/js/map.js
@@ -1,105 +1,36 @@
-jQuery(function () {
-    ymaps.ready(init);
-});
+ymaps.ready(init);
+var myMap;
 
 function init () {
-    var map = new ymaps.Map('map', {
-            center: [55.819543, 37.611619],
-            zoom: 10
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        markerElement = jQuery('#marker'),
-        dragger = new ymaps.util.Dragger({
-            // Драггер будет автоматически запускаться при нажатии на элемент 'marker'.
-            autoStartElement: markerElement[0]
-        }),
-        // Смещение маркера относительно курсора.
-        markerOffset,
-        markerPosition;
+    myMap = new ymaps.Map("map", {
+        center: [59.927301, 30.338456],
+        zoom: 16
+    }, {
+        balloonMaxWidth: 200,
+        searchControlProvider: 'yandex#search'
+    });
 
-    dragger.events
-        .add('start', onDraggerStart)
-        .add('move', onDraggerMove)
-        .add('stop', onDraggerEnd);
-
-    function onDraggerStart(event) {
-        var offset = markerElement.offset(),
-            position = event.get('position');
-        // Сохраняем смещение маркера относительно точки начала драга.
-        markerOffset = [
-            position[0] - offset.left,
-            position[1] - offset.top
-        ];
-        markerPosition = [
-            position[0] - markerOffset[0],
-            position[1] - markerOffset[1]
-        ];
-
-        applyMarkerPosition();
-    }
-
-    function onDraggerMove(event) {
-        applyDelta(event);
-    }
-
-    function onDraggerEnd(event) {
-        applyDelta(event);
-        markerPosition[0] += markerOffset[0];
-        markerPosition[1] += markerOffset[1];
-        // Переводим координаты страницы в глобальные пиксельные координаты.
-        var markerGlobalPosition = map.converter.pageToGlobal(markerPosition),
-            // Получаем центр карты в глобальных пиксельных координатах.
-            mapGlobalPixelCenter = map.getGlobalPixelCenter(),
-            // Получением размер контейнера карты на странице.
-            mapContainerSize = map.container.getSize(),
-            mapContainerHalfSize = [mapContainerSize[0] / 2, mapContainerSize[1] / 2],
-            // Вычисляем границы карты в глобальных пиксельных координатах.
-            mapGlobalPixelBounds = [
-                [mapGlobalPixelCenter[0] - mapContainerHalfSize[0], mapGlobalPixelCenter[1] - mapContainerHalfSize[1]],
-                [mapGlobalPixelCenter[0] + mapContainerHalfSize[0], mapGlobalPixelCenter[1] + mapContainerHalfSize[1]]
-            ];
-        // Проверяем, что завершение работы драггера произошло в видимой области карты.
-        if (containsPoint(mapGlobalPixelBounds, markerGlobalPosition)) {
-            // Теперь переводим глобальные пиксельные координаты в геокоординаты с учетом текущего уровня масштабирования карты.
-            var geoPosition = map.options.get('projection').fromGlobalPixels(markerGlobalPosition, map.getZoom()),
-            // Получаем уровень зума карты.
-                zoom = map.getZoom(),
-            // Получаем координаты тайла.
-                tileCoordinates = getTileCoordinate(markerGlobalPosition, zoom, 256);
-            alert([
-                'Координаты: ' + geoPosition,
-                'Уровень зума: ' + zoom,
-                'Глобальные пиксельные координаты: ' + markerGlobalPosition,
-                'Координаты тайла: ' + tileCoordinates
-            ].join(' '));
+    myMap.events.add('click', function (e) {
+        if (!myMap.balloon.isOpen()) {
+            var coords = e.get('coords');
+            myMap.balloon.open(coords, {
+                contentBody:'<p>Кое-кто щелкнул по карте!</p>'
+            });
+            document.getElementById('geo_inp').value = [
+                    coords[0].toPrecision(6),
+                    coords[1].toPrecision(6)
+                    ].join(', ');
         }
-    }
+        else {
+            myMap.balloon.close();
+        }
+    });
 
-    function applyDelta (event) {
-        // Поле 'delta' содержит разницу между положениями текущего и предыдущего события драггера.
-        var delta = event.get('delta');
-        markerPosition[0] += delta[0];
-        markerPosition[1] += delta[1];
-        applyMarkerPosition();
-    }
+    myMap.events.add('contextmenu', function (e) {
+        myMap.hint.open(e.get('coords'), 'Кто-то щелкнул правой кнопкой');
+    });
 
-    function applyMarkerPosition () {
-        markerElement.css({
-            left: markerPosition[0],
-            top: markerPosition[1]
-        });
-    }
-
-    function containsPoint (bounds, point) {
-        return point[0] >= bounds[0][0] && point[0] <= bounds[1][0] &&
-               point[1] >= bounds[0][1] && point[1] <= bounds[1][1];
-    }
-
-    function getTileCoordinate(coords, zoom, tileSize){
-        return [
-            Math.floor(coords[0] * zoom / tileSize),
-            Math.floor(coords[1] * zoom / tileSize)
-        ];
-    }
+    myMap.events.add('balloonopen', function (e) {
+        myMap.hint.close();
+    });
 }
